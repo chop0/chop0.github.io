3d5170c7987899953dfe3c9251d8b9ceca6a4ea0
alec <chop@quenda.net>
2024-07-07 18:22:01 -0400

    switch to d2d

---
+++ index_1.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<title>Demo2Design</title>
+<link href="styles.css" rel="stylesheet" type="text/css" />
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div align="center">
+  <table width="999" border="0">
+    <tr>
+      <th width="993" scope="col"><ul id="MenuBar1" class="MenuBarHorizontal">
+      <li>
+        <div align="center"><a href="index.php">Home</a></div>
+      </li>
+      <li><a href="Members2011.php" class="MenuBarItemSubmenu">Members</a>
+        <ul>
+          <li><a href="generalbody2011.php">General Body</a></li>
+          <li><a href="hotm.php">Hermano Of The Month</a></li>
+          <li><a href="hours2011.php" class="MenuBarItemSubmenu">Hours</a>
+            <ul>
+              <li><a href="hours.pdf">Academic</a></li>
+              <li><a href="communityHours Sheet1.pdf">Community Service</a></li>
+              <li><a href="Internal Social Hours (Fall 2011).pdf">Social</a></li>
+            </ul>
+          </li>
+<li><a href="HU Fitness.pdf">HU Fitness</a></li>
+        </ul>
+      </li>
+      <li><a href="history2011.php">History</a></li>
+      <li><a href="#" class="MenuBarItemSubmenu">Pillars</a>
+        <ul>
+          <li><a href="Academics2011.php" class="MenuBarItemSubmenu">Academics</a>
+            <ul>
+<li><a href="Hermanos Unidos de UC Santa Barbara Library.pdf">Library</a></li>
+<li><a href="AcademicBuddieHours Sheet1(2).pdf">Buddy Hours</a></li>
+<li><a href="AcademicOfficeHours Sheet1.pdf">Office Hours</a></li>
+<li><a href="#">HU Scholarship</a></li>
+            </ul>
+          </li>
+          <li><a href="Community2011.php">Community Service</a></li>
+          <li><a href="Social2011.php">Social</a></li>
+        </ul>
+      </li>
+      <li><a href="Events2011.php">Events</a></li>
+      <li><a href="clop.php">C.L.O.P.</a></li>
+<li><a href="#">Guest</a></li>
+      <li><a href="gallery.php">Gallery</a></li>
+      <li><a href="#">Videos</a></li>
+    </ul></th>
+    </tr>
+  </table>
+</div>
+<table width="900" border="0" align="center">
+  <tr>
+    <td align="center"><p><img name="" src="Home3.jpg" width="900" height="590" alt="" /><br />
+    </p></td>
+  </tr>
+  <tr>
+    <td align="center" class="footer">All material copyright &copy; Demo2Design 2015. All rights reserved.</td>
+  </tr>
+</table>
+<script type="text/javascript">
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+</script>
+</body>
+</html>
